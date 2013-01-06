8e60f243d24ed17285c879c1b4d4766913abd3f0
Gerold Penz <gerold@halvar.at>
2013-01-06 19:55:51 +0100

    ...

---
+++ examples/php_server_with_staticfiles/php_server_with_staticfiles.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#coding: utf-8
+"""
+Simple PHP-Server Example
+
+Links to the PHP pages:
+
+- http://localhost:8080/simple_page.php
+- http://localhost:8080/phpinfo.php
+
+"""
+
+import os
+import sys
+import cherrypy
+
+THISDIR = os.path.dirname(os.path.abspath(__file__))
+# Add app dir to path for testing cpcgiserver
+APPDDIR = os.path.abspath(os.path.join(THISDIR, os.path.pardir, os.path.pardir))
+sys.path.insert(0, APPDDIR)
+
+import cpcgiserver
+
+
+def main():
+
+    config = {
+        "global": {
+            # Server settings
+            "server.socket_host": "0.0.0.0",
+            "server.socket_port": 8080,
+        },
+        "/": {
+            # Enable CgiServer
+            "tools.cgiserver.on": True,
+            # Directory with PHP files
+            "tools.cgiserver.dir": THISDIR,
+            # URL for directory with PHP files
+            "tools.cgiserver.base_url": "/",
+            # Connect PHP extension with PHP interpreter program
+            "tools.cgiserver.handlers": {".php": "/usr/bin/php-cgi"},
+            # Staticdir
+            "tools.staticdir.on": True,
+            "tools.staticdir.dir": THISDIR,
+            "tools.staticdir.match": r"(?i)(gif|jpg|png|jpeg|js|7z|pdf|zip|svg|emf|avi|ods|css|ico|html|htm|p3p|swf|htc)$",
+        }
+    }
+
+    # Create and start application
+    app = cherrypy.Application(None, config = config)
+    cherrypy.quickstart(app, config = config)
+
+
+if __name__ == "__main__":
+    main()
+
--- examples/php_server_with_staticfiles/simple_page.php
+++ examples/php_server_with_staticfiles/simple_page.php
@@ -1,11 +1,11 @@
 <?php
   $name = 'CherryPy CGI PHP World';
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html lang="en">
 <head>
+  <meta charset="utf-8">
   <title>Simple PHP-Page</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <style type="text/css">
     body {
       font-family: sans-serif;
@@ -13,9 +13,8 @@
   </style>
 </head>
 <body>
-
   <div style="float: right; margin-left: 3em; margin-bottom: 3em;">
-    <img src="../php_files/cherrypy.png" alt="CherryPy" />
+    <img src="images/cherrypy.png" alt="CherryPy" />
   </div>
 
   <h1>Simple PHP-Page</h1>
@@ -45,6 +44,5 @@
   </p>
 
   <div style="clear: both"></div>
-
 </body>
 </html>
